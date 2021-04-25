cdbf731abeb372d7c56df7ffb7bd3945d6fee796
Ryan Yeske <ryan@ryanyeske.com>
2021-04-24 20:02:26 -0700

    add .emacs

--- home.nix
+++ home.nix
@@ -13,6 +13,8 @@
     pkgs.tmux
   ];
 
+  home.file.".emacs".source = ./.emacs;
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
